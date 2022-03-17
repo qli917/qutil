aeeb1522b1871e680c7c662e1673bf924dc46439
liqu <liqu917@163.com>
2022-03-17 11:34:20 +0800

    添加第三方库

--- app/src/main/java/com/q/qutil/MainActivity.kt
+++ app/src/main/java/com/q/qutil/MainActivity.kt
@@ -2,6 +2,7 @@ package com.q.qutil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.greenrobot.eventbus.EventBus
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
